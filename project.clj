42f94fee509f56f457716fa7aecc15865607ecfb
jarppe <jarppe@metosin.fi>
2013-08-14 09:11:52 +0300

    enabled 'lein run'

--- project.clj
+++ project.clj
@@ -2,6 +2,8 @@
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]]
                    :plugins [[lein-midje "3.1.1"]]}}
+  :resource-paths ["./LeapSDK/lib/LeapJava.jar"]
+  :jvm-opts ["-Djava.library.path=./LeapSDK/lib"]
   :main ^:skip-aot sormilla.main
   :repl-options {:init-ns sormilla.main}
   :min-lein-version "2.0.0")
