ec633eff87f7a533a0a7aee497aeccbc347b0f30
jarppe <jarppe@metosin.fi>
2013-08-17 23:52:15 +0300

    refactored drawing

--- src/sormilla/core.clj
+++ src/sormilla/core.clj
@@ -2,7 +2,7 @@
   (:require [sormilla.gui :as gui]
             [sormilla.leap :as leap]
             [sormilla.math :as math])
-  (:import [java.awt Color Graphics2D RenderingHints Rectangle]
+  (:import [java.awt Color Graphics2D RenderingHints Rectangle BasicStroke]
            [java.awt.geom Ellipse2D$Double]))
 
 (set! *warn-on-reflection* true)
@@ -18,16 +18,21 @@
                            :lo       (Color.  255   64  64    64)}})
 
 (def quality-colors [(Color.  255   64  64   192)
-                     (Color.  192   64  64    64)
-                     (Color.  192   64  64    32)
-                     (Color.  192  192  64    32)
-                     (Color.   64  192  64    32)
-                     (Color.   64  192  64    64)])
+                     (Color.  255   64  64   192)
+                     (Color.  255   64  64   192)
+                     (Color.  192   64  64   128)
+                     (Color.   64  192  64   128)
+                     (Color.   64  192  64   255)])
+
+(def stroke-0 (BasicStroke. 0.0))
+(def stroke-1 (BasicStroke. 1.0))
 
 (defmacro with-trans [^Graphics2D g & body]
-  `(let [t# (.getTransform ~g)]
+  `(let [t# (.getTransform ~g)
+         c# (.getClip ~g)]
      ~@body
-     (.setTransform ~g t#)))
+     (.setTransform ~g t#)
+     (.setClip ~g c#)))
 
 (defn pitcher []
   (comp
@@ -54,67 +59,79 @@
   (let [pitch (pitcher)
         yaw (yawer)
         roll (roller)]
-    (fn [^Graphics2D g w h hand]
+    (fn [^Graphics2D g hand]
+      
+      ; clear background
+      (.setColor g background-color)
+      (.fillRect g 0 0 100 100)
+      
+      ; draw grid
+      (.setColor g hud-color)
+      (.setStroke g stroke-0)
+      (doseq [n (range -40 50 10)]
+        (.drawLine g -50 n 50 n)
+        (.drawLine g n -50 n 50))
+
+      ; draw zero axis
+      (.setColor g hud-hi-color)
+      (.drawLine g -50 0 50 0)
+      (.drawLine g 0 -50 0 50)
+      
+      ; draw quality boxes
+      (let [quality (:quality hand 0)]
+        (.setColor g (nth quality-colors quality))
+        (doseq [y (range 5)]
+          (when (>= y quality) (.setColor g hud-lo-color))
+          (.fillRect g -48 (- 48 (* y 3)) 8 2)))
+
       (when hand
-        (.setColor g (nth quality-colors (:quality hand)))
-        (doseq [n (range 5 (* 10 (:quality hand)) 10)]
-          (.fillRect g 5 n 23 8))
-        (.setColor g hud-color)
-        (doseq [n (range 5 50 10)]
-          (.drawRect g 5 n 23 5))
-        (.setColor g norm)
-        (.setClip g (Rectangle. 0 0 (/ w 2) h))
-        (with-trans g
-          (.translate g (double (/ w 4)) (double (/ h 2)))
-          (.scale g (double (/ (/ w 4) 100.0)) (double (/ (/ h 2) 100.0)))
-          (.translate g (double (yaw hand)) (double (pitch hand)))
-          (.rotate g (roll hand))
-          (.setColor g lo)
-          (.fillOval g -75 -10 150 20)
-          (.setColor g norm)
-          (.drawOval g -75 -10 150 20)
-          (.drawLine g -200 0 200 0)
-          (.drawLine g 0 -200 0 200))))))
+        (.translate g ^double (yaw hand) ^double (pitch hand))
+        (.rotate g (roll hand))
+        (.setStroke g stroke-1)
+        (.setColor g lo)
+        (.fillOval g -75 -10 150 20)
+        (.drawOval g -75 -10 150 20)
+        (.drawLine g -200 0 200 0)
+        (.drawLine g 0 -200 0 200)))))
 
 (def draw-left-hand (hand-drawer (:left hand-colors)))
 (def draw-right-hand (hand-drawer (:right hand-colors)))
 
 (defn render [^Graphics2D g ^long w ^long h frame]
+  (.setRenderingHint g RenderingHints/KEY_ANTIALIASING RenderingHints/VALUE_ANTIALIAS_ON)
   (.setColor g background-color)
   (.fillRect g 0 0 w h)
-  (.setRenderingHint g RenderingHints/KEY_ANTIALIASING RenderingHints/VALUE_ANTIALIAS_ON)
-  (let [[left-hand right-hand] frame]
+  (let [w2 (/ w 2.0)
+        w4 (/ w 4.0)
+        h2 (/ h 2.0)]
+    (with-trans g
+      (.translate g w4 h2)
+      (.scale g (/ w2 100.0) (/ h 100.0))
+      (.setClip g (Rectangle. -50 -50 100 100))
+      (draw-left-hand g (first frame)))
+    (with-trans g
+      (.translate g (+ w2 w4) h2)
+      (.scale g (/ w2 100.0) (/ h 100.0))
+      (.setClip g (Rectangle. -50 -50 100 100))
+      (draw-right-hand g (second frame)))
     (.setColor g hud-color)
-    (.drawLine g (/ w 2 ) 0 (/ w 2) h)
-    (.setColor g hud-lo-color)
-    (doseq [x (map (fn [v] (int (* v (/ w 20.0)))) (range 20))]
-      (.drawLine g x 0 x h))
-    (doseq [x (map (fn [v] (int (* v (/ w 100.0)))) (range 100))]
-      (.drawLine g x (- (/ h 2) 5) x (+ (/ h 2) 5)))
-    (doseq [y (map (fn [v] (int (* v (/ h 10.0)))) (range 10))]
-      (.drawLine g 0 y w y))
-    (doseq [y (map (fn [v] (int (* v (/ h 50.0)))) (range 50))]
-      (.drawLine g (- (/ w 4) 5) y (+ (/ w 4) 5) y)
-      (.drawLine g (- (* 3 (/ w 4)) 5) y (+ (* 3 (/ w 4)) 5) y))
-    (draw-left-hand g w h (nth frame 0))
-    (.setClip g (Rectangle. 0 0 (inc w) (inc h)))
-    (.translate g (double (/ w 2)) (double 0.0))
-    (draw-right-hand g w h (nth frame 1))))
+    (.setStroke g (BasicStroke. 1.0))
+    (.drawLine g w2 0 w2 h)))
 
 
 (defn dummy-source []
  
-  [{:quality    3
+  [{:quality    5
     :pitch      0.0
     :yaw        0.0
     :roll       0.0}
 
-   {:quality   0
+   {:quality   5
     :pitch     0.2
     :yaw       0.2
-    :roll     -0.0}]
+    :roll     -0.2}]
   
-  (leap/frame)
+  #_(leap/frame)
   
   )
 
