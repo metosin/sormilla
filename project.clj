9bc185a8561a4a2c9337fbf0c1103c8eaa47417b
jarppe <jarppe@metosin.fi>
2013-08-24 18:59:21 +0300

    opencv included

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
                  [amalloy/ring-buffer "1.0"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]]
                    :plugins [[lein-midje "3.1.1"]]}}
-  :resource-paths ["./LeapSDK/lib/LeapJava.jar"]
-  :jvm-opts ["-Djava.library.path=./LeapSDK/lib"]
+  :resource-paths ["./LeapSDK/lib/LeapJava.jar" "./opencv/opencv-246.jar"]
+  :jvm-opts ["-Djava.library.path=./LeapSDK/lib:./opencv"]
   :main ^:skip-aot sormilla.main
   :repl-options {:init-ns sormilla.main}
   :min-lein-version "2.0.0")
