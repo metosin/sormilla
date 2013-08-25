f13ad94c08688f2c28f29f585e41ea6d869810e9
jarppe <jarppe@metosin.fi>
2013-08-25 14:02:36 +0300

    cleanup

--- src/sormilla/video.clj
+++ src/sormilla/video.clj
@@ -11,51 +11,43 @@
            [org.apache.commons.io IOUtils])
   (:require [sormilla.bin :as bin]
             [sormilla.drone-comm :as comm]
+            [sormilla.system :refer [run?]]
             [clojure.java.io :as io]))
 
-; (set! *warn-on-reflection* true)
+(set! *warn-on-reflection* true)
 
 ;;
 ;; I/O
 ;;
 
-(defn read-input [^InputStream in buffer size]
+(defn read-input ^bytes [^InputStream in ^bytes buffer ^long size]
   (IOUtils/readFully in buffer 0 size)
   buffer)
 
-(defn skip-input [^InputStream in size]
+(defn skip-input [^InputStream in ^long size]
   (.skip in size)
   in)
-
-(defn init-video [s]
-  (doto (.getOutputStream s)
-    (.write (byte-array (map bin/ubyte [1 0 0 0])))
-    (.flush)))
-
-;;
-;; Video stream parsing
-;;
-
-(defn signature [header]    (bin/get-int header 0))
-(defn header-size [header]  (bin/get-short header 6))
-(defn payload-size [header] (bin/get-int header 8))
-
 
 ;;
 ;; Video decoding
 ;;
 
-(defn ba->ia [^bytes source ^ints target]
-  (doseq [i (range (count source))]
-    (aset-int target i (bit-and 0xFF (nth source i))))
+(defn ba->ia [^bytes source ^ints target ^long size]
+  (loop [i 0]
+    (aset-int target i (bit-and 0xFF (aget source i)))
+    (when (< (inc i) size) (recur (inc i))))
   target)
 
-(defn buffer->image [w h buffer]
+(defn signature [header]    (bin/get-int header 0))
+(defn header-size [header]  (bin/get-short header 6))
+(defn payload-size [header] (bin/get-int header 8))
+
+(defn buffer->image ^BufferedImage [^long w ^long h ^ints buffer]
   (let [image (BufferedImage. w h BufferedImage/TYPE_INT_RGB)]
     (.setRGB image 0 0 w h buffer 0 w)
     image))
 
-(defn make-decoder []
+(defn make-decoder [^InputStream in]
   (let [codec           (H264Decoder.)
         context         (MpegEncContext/avcodec_alloc_context)
         packet          (AVPacket.)
@@ -69,32 +61,56 @@
     (if (neg? (.avcodec_open context codec)) (throw (Exception. "Could not open codec")))
     (.av_init_packet packet)
     
-    (fn [in]
+    (fn ^BufferedImage []
       (let [header       (read-input in header-buffer 12)
             header-size  (header-size header)
             image-size   (payload-size header)]
-      
+        
         (when-not (= (signature header) 0x45566150) (throw (Exception. "out of sync")))
         (skip-input in (- header-size 12))
         (read-input in image-buffer image-size)
         
         (set! (.size packet) image-size)
-        (set! (.data_base packet) (ba->ia image-buffer ibuffer)) ; FIXME: size
+        (set! (.data_base packet) (ba->ia image-buffer ibuffer image-size))
         (set! (.data_offset packet) 0)
         
         (.avcodec_decode_video2 context frame got-picture? packet)
         (when (zero? (first got-picture?)) (throw (Exception. "Could not decode frame")))
         
-        (let [picture (.displayPicture (.priv_data context))
-              width (.imageWidth picture)
-              height (.imageHeight picture)
-              picture-buffer (int-array (* width height))]
+        (let [picture         (.displayPicture (.priv_data context))
+              width           (.imageWidth picture)
+              height          (.imageHeight picture)
+              picture-buffer  (int-array (* width height))]
           (FrameUtils/YUV2RGB picture picture-buffer)
           (buffer->image width height picture-buffer))))))
 
-(defn update-image [img]
-  (ImageIO/write img "png" (io/file "image.png")))
+;;
+;; Streaming:
+;;
 
+(defonce image (atom nil))
+
+(defn init-video-streaming! []
+  (comm/send-commands! [(comm/video-codec :h264-360p) (comm/video-frame-rate 15)])
+  (let [socket   (doto (Socket.)
+                   (.setSoTimeout 2000)
+                   (.connect (InetSocketAddress. comm/drone-ip 5555)))
+        decoder  (make-decoder (io/input-stream socket))]
+    (doto (.getOutputStream socket)
+      (.write (byte-array (map bin/ubyte [1 0 0 0])))
+      (.flush))
+    (future
+      (try
+        (while (run?)
+          (reset! image (decoder)))
+        (catch Exception e
+          (println "exception while processing video stream" e)
+          (.printStackTrace e))
+        (finally
+          (try (.close socket) (catch Exception e))
+          (reset! image nil))))))
+
+(comment
 
 (defn parse-file []
   (let [in (io/input-stream (io/file "capture.h264"))
@@ -109,13 +125,10 @@
       (finally
         (try (.close in) (catch Exception e))))))
 
+(parse-file)
 
-(comment
+; ffmpeg -f h264 -an -i capture.h264 stream.m4v
 
-  (parse-file)
-
-  ; ffmpeg -f h264 -an -i capture.h264 stream.m4v
-  
 (defn capture []
   (let [socket (doto (Socket.)
                  (.setSoTimeout 2000)
@@ -135,36 +148,5 @@
         (try (.close socket) (catch Exception e))
         (try (.close out) (catch Exception e))))))
 
-  (comm/send-commands! [(comm/video-codec :h264-360p) (comm/video-frame-rate 15) comm/leds-active])
-  (capture)
-  (comm/send-commands! [comm/leds-reset])
-
-(defn parse-file []
-  (let [in (io/input-stream (io/file "capture.h264"))]
-    (try
-      (doseq [i (range 1)]
-        (let [header-data  (read-input in 12)
-              header       (parse-header header-data)]
-          (when-not (= (:sig header) 0x45566150) (throw (Exception. "out of sync")))
-          (skip-input in (- (:header-size header) 12))
-          (let [frame (read-input in (:payload-size header))
-                img (frame->image frame)]
-            (update-image img))))
-      (println "success!")
-      (catch Exception e
-        (println "failure" e)
-        (.printStackTrace e))
-      (finally
-        (try (.close in) (catch Exception e))))))
-  
 )
 
-#_(doseq [n (range 100)]
-          (let [header-data  (read-input in 68)
-                signature    (parse-signature header-data)
-                header       (parse-header header-data)]
-            (if (= signature "PaVE")
-              (do
-                (.write out header-data)
-                (.write out (read-input in (:payload-size header))))
-              (println "not a pave" n))))
--- test/sormilla/video_test.clj
+++ test/sormilla/video_test.clj
@@ -4,16 +4,9 @@
             [midje.sweet :refer :all]))
 
 (facts ba->ia
+  
   (let [source (byte-array (map ubyte [0x00 0x55 0x88 0xFF]))
         target (int-array 4)
         expected (int-array (map uint [0x00 0x55 0x88 0xFF]))]
-    (ba->ia source target)
+    (ba->ia source target 4)
     (java.util.Arrays/equals target expected) => true))
-
-(facts convert!
-  (not (zero? (first (int-array [0])))) => false
-  (not (zero? (first (int-array [1])))) => true)
-
-(facts read-signature
-  (read-signature (byte-array (map ubyte [65 66 67 68]))) => "ABCD")
-
