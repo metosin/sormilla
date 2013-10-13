9085bc0656f000e7cb5a0dc961ec91ddfd73ae47
jarppe <jarppe@metosin.fi>
2013-10-13 14:33:53 +0300

    soem common i/o stuff

---
+++ src/sormilla/io_utils.clj
@@ -0,0 +1,27 @@
+(ns sormilla.io-utils
+  (:require [sormilla.bin :as bin])
+  (:import [java.io InputStream]
+           [java.net Socket InetAddress InetSocketAddress]
+           [org.apache.commons.io IOUtils]))
+
+(defn read-fully [^InputStream in ^bytes buffer ^long offset ^long size]
+  (IOUtils/readFully in buffer offset size)
+  buffer)
+
+(defn skip-fully [^InputStream in ^long size]
+  (IOUtils/skipFully in size))
+
+(defn open-socket ^Socket [^InetAddress addr port]
+  (doto (Socket.)
+    (.setSoTimeout 2000)
+    (.connect (InetSocketAddress. addr port))))
+
+(defn write-to-socket ^Socket [^Socket socket & data]
+  (doto (.getOutputStream socket)
+    (.write (byte-array (map bin/ubyte data)))
+    (.flush))
+  socket)
+
+(defn open-video-socket ^Socket [^InetAddress addr]
+  (doto (open-socket addr 5555)
+    (write-to-socket 1 0 0 0)))
