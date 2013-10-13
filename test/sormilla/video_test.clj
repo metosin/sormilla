f3f5652e35bb3a2c0448659fc748dc90bbc50a9c
jarppe <jarppe@metosin.fi>
2013-10-13 14:35:37 +0300

    cleanup

--- test/sormilla/video_test.clj
+++ test/sormilla/video_test.clj
@@ -3,9 +3,4 @@
             [sormilla.bin :refer [ubyte uint]]
             [midje.sweet :refer :all]))
 
-(facts ba->ia
-  (let [source (byte-array (map ubyte [0x00 0x55 0x88 0xFF]))
-        target (int-array 4)
-        expected (int-array (map uint [0x00 0x55 0x88 0xFF]))]
-    (ba->ia source target 4)
-    (java.util.Arrays/equals target expected) => true))
+
