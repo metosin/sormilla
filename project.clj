998d33aab3085e667dc28223cdf68e87462b82b3
Juho Teperi <juho.teperi@iki.fi>
2015-11-16 19:01:23 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -1,17 +1,17 @@
 (defproject sormilla "0.4.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.async "0.1.242.0-44b1e3-alpha"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/core.async "0.2.374"]
                  [metosin/leaplib "2.3.1"]
                  [metosin/leaplib-natives "2.3.1"]
                  [h264-decoder/h264-decoder "1.0"]
-                 [amalloy/ring-buffer "1.0"]
+                 [amalloy/ring-buffer "1.2"]
                  [commons-io/commons-io "2.4"]
                  [metosin/system "0.2.1"]]
   :profiles {:dev {:source-paths ["dev"]
-                   :dependencies [[midje "1.5.1"]
-                                  [org.clojure/tools.namespace "0.2.4"]
-                                  [org.clojure/java.classpath "0.2.0"]]
-                   :plugins [[lein-midje "3.1.1"]]}
+                   :dependencies [[midje "1.8.2"]
+                                  [org.clojure/tools.namespace "0.2.10"]
+                                  [org.clojure/java.classpath "0.2.3"]]
+                   :plugins [[lein-midje "3.2"]]}
              :uberjar {:source-paths ["main"]
                        :main sormilla.main
                        :aot [sormilla.main]}
