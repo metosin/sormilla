5cb55fbb0a6eff8f76fd396215c70514ad18aa41
Juho Teperi <juho.teperi@iki.fi>
2015-11-16 19:01:09 +0200

    Remove old test

--- test/sormilla/drone_comm_test.clj
+++ test/sormilla/drone_comm_test.clj
@@ -1,10 +1,3 @@
 (ns sormilla.drone-comm-test
   (:require [sormilla.drone-comm :refer :all]
             [midje.sweet :refer :all]))
-
-(facts "parse-nav-state"
-  (parse-nav-state  0x00000000) => (contains {:flying :landed :emergency :ok})
-  (parse-nav-state  0x00000001) => (contains {:flying :flying :emergency :ok})
-  (parse-nav-state  0x80000000) => (contains {:flying :landed :emergency :detected})
-  (parse-nav-state  0x80000001) => (contains {:flying :flying :emergency :detected}))
-
